c58860e30432ecc0bce7a091127675362be2e33a
Yuping Huang <yupinghyper@gmail.com>
2023-12-15 02:58:27 +0000

    Initial commit.

---
+++ launch_server.sh
@@ -0,0 +1,1 @@
+text-generation-launcher --model-id meta-llama/Llama-2-13b-chat-hf --huggingface-hub-cache /data00/yuping/huggingface-cache --max-batch-total-tokens 2048 --max-batch-prefill-tokens 2048 --max-total-tokens 2048 --max-input-length 1024 --max-concurrent-requests 3 --hostname=0.0.0.0
